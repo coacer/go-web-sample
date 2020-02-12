f0fd0f455091fb54976336f998ef8fa888a3ff23
coacer <coacer728h23@icloud.com>
2020-02-12 21:35:39 +0900

    Finish

--- api/main.go
+++ api/main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"github.com/coacer/go-web-sample/api/db"
-	"github.com/gin-gonic/gin"
+	"github.com/coacer/go-web-sample/api/server"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello World")
-	})
 	db.Init()
-	r.Run()
-	db.Close()
+	server.Init()
 }
--- api/server/server.go
+++ api/server/server.go
@@ -5,6 +5,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func Init() {
+	r := router()
+	r.Run()
+}
+
 func router() *gin.Engine {
 	r := gin.Default()
 
