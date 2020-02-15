fe4fe09fe52de2c58cf4f617f40362a5aa4e0d43
coacer <coacer728h23@icloud.com>
2020-02-15 10:10:06 +0900

    [api] Add timestamps

--- api/model/entity/post.go
+++ api/model/entity/post.go
@@ -1,7 +1,11 @@
 package entity
 
+import "time"
+
 type Post struct {
-	ID    uint   `json:"id"`
-	Title string `json:"title"`
-	Body  string `json:"body"`
+	ID        uint      `json:"id"`
+	Title     string    `json:"title"`
+	Body      string    `json:"body"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
