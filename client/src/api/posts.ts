429609389c1253bbaa7b5bb544accae3dac871ca
coacer <coacer728h23@icloud.com>
2020-02-15 09:13:52 +0900

    [client] Finish delete function

--- client/src/api/posts.ts
+++ client/src/api/posts.ts
@@ -2,11 +2,27 @@ import axios from '../plugins/axios';
 import { Post } from '../interfaces';
 
 export const fetchPostAPI = async (): Promise<Post[]> => {
-  const { data } = await axios.get('/posts');
-  return data;
+  try {
+    const { data } = await axios.get('/posts');
+    return data;
+  } catch (e) {
+    console.log(e.message);
+  }
 };
 
 export const addPostAPI = async ({ title, body }): Promise<Post> => {
-  const { data } = await axios.post('/posts', { title, body });
-  return data;
+  try {
+    const { data } = await axios.post('/posts', { title, body });
+    return data;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+export const deletePostAPI = async (id: number): Promise<void> => {
+  try {
+    await axios.delete(`/posts/${id}`);
+  } catch (e) {
+    console.log(e.message);
+  }
 };
--- client/src/components/posts/PostListItem.tsx
+++ client/src/components/posts/PostListItem.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../../contexts';
 import { Post } from '../../interfaces';
 import SimpleCard from '../atoms/SimpleCard';
 import styled from 'styled-components';
 import { IconButton } from '@material-ui/core';
 import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
-// import { deletePost } from '../../store/actions/posts';
+import { deletePost } from '../../store/actions/posts';
+import { deletePostAPI } from '../../api/posts';
 
 interface Props {
   post: Post;
@@ -13,11 +15,13 @@ interface Props {
 export type PostListItemProps = Props;
 
 const PostListItem: React.FC<Props> = ({ post }: Props) => {
-  // const handleClick = (id: number): void => {
-  //   deletePostAPI(id)
-  //     .then(res => dispatch(deletePost(id)))
-  //     .catch(e => console.log(e.message));
-  // };
+  const { dispatch } = useContext(AppContext);
+
+  const handleClick = (id: number): void => {
+    deletePostAPI(id)
+      .then(res => dispatch(deletePost(id)))
+      .catch(e => console.log(e.message));
+  };
 
   return (
     <Wrapper key={post.id}>
@@ -25,7 +29,7 @@ const PostListItem: React.FC<Props> = ({ post }: Props) => {
         title={post.title}
         body={post.body}
         rightBtn={
-          <IconButton>
+          <IconButton onClick={e => handleClick(post.id)}>
             <DeleteRoundedIcon color="secondary" fontSize="large" />
           </IconButton>
         }
