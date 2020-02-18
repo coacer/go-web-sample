63301d4829b86786647bb9589aa8fd8739fd9abb
coacer <coacer728h23@icloud.com>
2020-02-18 13:20:13 +0900

    [client] Add transition

--- client/src/components/posts/PostList.tsx
+++ client/src/components/posts/PostList.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Post } from '../../interfaces';
 import PostListItem from './PostListItem';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import styled from 'styled-components';
 
 interface Props {
   posts: Post[];
@@ -10,12 +12,38 @@ export type PostListProps = Props;
 
 const PostList: React.FC<Props> = ({ posts }: Props) => {
   return (
-    <>
+    <FadeWrapper>
       {posts.map(post => (
-        <PostListItem key={post.id} post={post} />
+        <CSSTransition key={post.id} timeout={500} classNames="fade">
+          <PostListItem post={post} />
+        </CSSTransition>
       ))}
-    </>
+    </FadeWrapper>
   );
 };
+
+const FadeWrapper = styled(TransitionGroup)`
+  .fade-enter {
+    opacity: 0;
+    transform: translate(-100px);
+  }
+
+  .fade-enter-active {
+    opacity: 1;
+    transform: translate(0);
+    transition: 0.5s ease;
+  }
+
+  .fade-exit {
+    opacity: 1;
+    transform: translate(0);
+    transition: 0.5s ease;
+  }
+
+  .fade-exit-active {
+    opacity: 0;
+    transform: translate(100px);
+  }
+`;
 
 export default PostList;
